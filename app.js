2eaed1d7eae4014182d1c4641d50448ca9854b41
Aakash Darsi <darsisriguruaakash@gmail.com>
2025-10-17 04:09:32 +0530

    added ratelimiting & fingerprinting

--- app.js
+++ app.js
@@ -6,27 +6,39 @@ const stripeRouter = require("./routes/stripe");
 const cors = require("cors");
 const app = express();
 const tracer = require("dd-trace");
+const { rateLimit } = require("express-rate-limit");
+const helmet = require("helmet");
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://your-frontend-app.com",
   "https://ninjaudemy.aakashdarsi.online",
 ];
 
-tracer.init();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: false,
+  legacyHeaders: false,
+  ipv6Subnet: 56,
+});
 
+tracer.init();
+app.use(limiter);
+app.disable("x-powered-by");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.use("/customers", customerRouter);
 app.use("/cart", cartRouter);
 app.use("/products", productRoutes);
 app.use("/pay", stripeRouter);
 
+// Disabling for the fingerprinting
+
 app.use("/health", (req, res, next) => {
   res.send("End Point working");
-  next();
 });
 
 app.use((req, res) => {
